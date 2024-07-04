8ac8808954b8bddb8983d87495686a5671a2d935
Ahda Rindang Al-Amin <ahdaalamin2506@gmail.com>
2024-07-04 15:54:23 +0700

    so

--- src/tesUAPkedua/sorting.java
+++ src/tesUAPkedua/sorting.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class sorting {
-    // Class to hold input-output pairs
+
     public static class InputOutputPair {
         String input;
         int output;
@@ -33,27 +33,8 @@ public class sorting {
                 }
             }
 
-            // If no two elements were swapped by inner loop, then break
             if (!swapped) break;
         }
     }
-
-/*    public static void main(String[] args) {
-        List<InputOutputPair> pairs = new ArrayList<>();
-
-        // Example data
-        pairs.add(new InputOutputPair("5 1 2 + 4 * + 3 -", 14));
-        pairs.add(new InputOutputPair("3 4 + 2 * 7 /", 2));
-        pairs.add(new InputOutputPair("5 6 2 + *", 40));
-        pairs.add(new InputOutputPair("8 5 * 7 +", 47));
-
-        // Sort the pairs by output
-        bubbleSort(pairs);
-
-        // Print sorted pairs
-        for (InputOutputPair pair : pairs) {
-            System.out.println("Input: " + pair.input + ", Output: " + pair.output);
-        }
-    } */
 }
 
